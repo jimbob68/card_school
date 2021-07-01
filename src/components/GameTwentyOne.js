07a7190859b72ad197d0ee0679eb7662808dade8
James Fraser <jimbob4t@virginmedia.com>
2021-07-01 13:16:28 +0100

    refactored code and took out older commented out code

--- src/components/GameTwentyOne.js
+++ src/components/GameTwentyOne.js
@@ -85,8 +85,13 @@ const GameTwentyOne = () => {
 			.then((results) => {
 				setDeckId(results.deck_id);
 				console.log('deck info', results);
+				return results.deck_id;
 			})
-			.then(() => console.log('deck_id:', deckId));
+			.then((deck_id) => {
+				fetch('https://deckofcardsapi.com/api/deck/' + deck_id + '/draw/?count=416')
+					.then((res) => res.json())
+					.then((results) => setDeckOfCards(results.cards));
+			});
 	}, []);
 
 	useEffect(
@@ -96,12 +101,6 @@ const GameTwentyOne = () => {
 				const cardDrawn = handleTwist(0);
 				cards.push(cardDrawn);
 				setComputerScore(calculateScore(cards));
-
-				// handleTwist(0).then((res) => {
-				// 	console.log(res);
-				// 	computerHand.push(res.cards[0]);
-				// 	setComputerScore(calculateScore(computerHand));
-				// });
 			} else if (playerNumberTurn === 0) {
 				setTimeout(() => {
 					handleEndRound();
@@ -199,91 +198,32 @@ const GameTwentyOne = () => {
 
 	const handleDrawCards = () => {
 		setPlayerNumberTurn(1);
-		if (deckOfCards.length === 0) {
-			fetch('https://deckofcardsapi.com/api/deck/' + deckId + '/draw/?count=416')
-				.then((res) => res.json())
-				.then((results) => {
-					let deck = results.cards;
-					setComputerHand(deck.slice(0, 2));
-					setPlayerOneHand(deck.slice(2, 4));
-					setPlayerOneBet(5);
-					setPlayerOneWallet(playerOneWallet - 5);
-					if (numberOfPlayers === 1) {
-						setDeckOfCards(deck.slice(4));
-					}
-					if (numberOfPlayers === 2) {
-						setPlayerTwoBet(5);
-						setPlayerTwoWallet(playerTwoWallet - 5);
-						setPlayerTwoHand(deck.slice(4, 6));
-						setDeckOfCards(deck.slice(6));
-					}
-					if (numberOfPlayers === 3) {
-						setPlayerTwoBet(5);
-						setPlayerTwoWallet(playerTwoWallet - 5);
-						setPlayerThreeBet(5);
-						setPlayerThreeWallet(playerThreeWallet - 5);
-						setPlayerTwoHand(deck.slice(4, 6));
-						setPlayerThreeHand(deck.slice(6, 8));
-						setDeckOfCards(deck.slice(8));
-					}
-					if (numberOfPlayers === 4) {
-						setPlayerTwoBet(5);
-						setPlayerTwoWallet(playerTwoWallet - 5);
-						setPlayerThreeBet(5);
-						setPlayerThreeWallet(playerThreeWallet - 5);
-						setPlayerFourBet(5);
-						setPlayerFourWallet(playerFourWallet - 5);
-						setPlayerTwoHand(deck.slice(4, 6));
-						setPlayerThreeHand(deck.slice(6, 8));
-						setPlayerFourHand(deck.slice(8, 10));
-						setDeckOfCards(deck.slice(10));
-					}
+		setPlayerOneSplitHand([]);
+		setPlayerTwoSplitHand([]);
+		setPlayerThreeSplitHand([]);
+		setPlayerFourSplitHand([]);
+		let deck = deckOfCards;
+		setComputerHand(deck.slice(0, 2));
+		setPlayerOneHand(deck.slice(2, 4));
+		setPlayerOneBet(5);
+		setPlayerOneWallet(playerOneWallet - 5);
 
-					// setPlayerTwoHand(results.cards.slice(2, 4));
-					// setComputerHand(results.cards.slice(4));
-				});
-		} else {
-			setPlayerOneSplitHand([]);
-			setPlayerTwoSplitHand([]);
-			setPlayerThreeSplitHand([]);
-			setPlayerFourSplitHand([]);
-			let deck = deckOfCards;
-			setComputerHand(deck.slice(0, 2));
-			setPlayerOneHand(deck.slice(2, 4));
-			setPlayerOneBet(5);
-			setPlayerOneWallet(playerOneWallet - 5);
-
-			if (numberOfPlayers === 1) {
-				setDeckOfCards(deck.slice(4));
-			}
-			if (numberOfPlayers === 2) {
-				setPlayerTwoBet(5);
-				setPlayerTwoWallet(playerTwoWallet - 5);
-				setPlayerTwoHand(deck.slice(4, 6));
-				setDeckOfCards(deck.slice(6));
-			}
-			if (numberOfPlayers === 3) {
-				setPlayerTwoBet(5);
-				setPlayerTwoWallet(playerTwoWallet - 5);
-				setPlayerThreeBet(5);
-				setPlayerThreeWallet(playerThreeWallet - 5);
-				setPlayerTwoHand(deck.slice(4, 6));
-				setPlayerThreeHand(deck.slice(6, 8));
-				setDeckOfCards(deck.slice(8));
-			}
-			if (numberOfPlayers === 4) {
-				setPlayerTwoBet(5);
-				setPlayerTwoWallet(playerTwoWallet - 5);
-				setPlayerThreeBet(5);
-				setPlayerThreeWallet(playerThreeWallet - 5);
-				setPlayerFourBet(5);
-				setPlayerFourWallet(playerFourWallet - 5);
-				setPlayerTwoHand(deck.slice(4, 6));
-				setPlayerThreeHand(deck.slice(6, 8));
-				setPlayerFourHand(deck.slice(8, 10));
-				setDeckOfCards(deck.slice(10));
-			}
+		if (numberOfPlayers >= 2) {
+			setPlayerTwoBet(5);
+			setPlayerTwoWallet(playerTwoWallet - 5);
+			setPlayerTwoHand(deck.slice(4, 6));
 		}
+		if (numberOfPlayers >= 3) {
+			setPlayerThreeBet(5);
+			setPlayerThreeWallet(playerThreeWallet - 5);
+			setPlayerThreeHand(deck.slice(6, 8));
+		}
+		if (numberOfPlayers >= 4) {
+			setPlayerFourBet(5);
+			setPlayerFourWallet(playerFourWallet - 5);
+			setPlayerFourHand(deck.slice(8, 10));
+		}
+		setDeckOfCards(deck.slice(numberOfPlayers * 2 + 2));
 	};
 
 	const handleSplit = (playerNumber) => {
@@ -292,8 +232,6 @@ const GameTwentyOne = () => {
 			setPlayerOneHand([ playerOneHand[0] ]);
 			setPlayerOneBet(playerOneBet * 2);
 			setPlayerOneWallet(playerOneWallet - playerOneBet);
-			// handleTwist(1);
-			// handleTwist(1.5);
 		} else if (playerNumber === 2) {
 			setPlayerTwoSplitHand([ playerTwoHand[1] ]);
 			setPlayerTwoHand([ playerTwoHand[0] ]);
@@ -312,40 +250,6 @@ const GameTwentyOne = () => {
 		}
 	};
 
-	// const displayCards = (hand, playerNumber) => {
-	// 	const cardImages = hand.map((card) => <img src={card.image} alt={card.code} />);
-
-	// 	if (hand.length === 2 && hand[0].value === hand[1].value && playerNumberTurn !== 0) {
-	// 		cardImages.push(<button onClick={() => handleSplit(playerNumberTurn)}>Split</button>);
-	// 	}
-	// 	if (playerNumber === 1 && playerOneSplitHand.length > 0) {
-	// 		cardImages.push(<p>first split hand score:{calculateScore(playerOneHand, false)}</p>);
-	// 		playerOneSplitHand.forEach((card) => {
-	// 			cardImages.push(<img src={card.image} alt={card.code} />);
-	// 		});
-	// 		cardImages.push(<p>second split hand score:{calculateScore(playerOneSplitHand, false)}</p>);
-	// 	} else if (playerNumber === 2 && playerTwoSplitHand.length > 0) {
-	// 		cardImages.push(<p>first split hand score:{calculateScore(playerTwoHand, false)}</p>);
-	// 		playerTwoSplitHand.forEach((card) => {
-	// 			cardImages.push(<img src={card.image} alt={card.code} />);
-	// 		});
-	// 		cardImages.push(<p>second split hand score:{calculateScore(playerTwoSplitHand, false)}</p>);
-	// 	} else if (playerNumber === 3 && playerThreeSplitHand.length > 0) {
-	// 		cardImages.push(<p>first split hand score:{calculateScore(playerThreeHand, false)}</p>);
-	// 		playerThreeSplitHand.forEach((card) => {
-	// 			cardImages.push(<img src={card.image} alt={card.code} />);
-	// 		});
-	// 		cardImages.push(<p>second split hand score:{calculateScore(playerThreeSplitHand, false)}</p>);
-	// 	} else if (playerNumber === 4 && playerFourSplitHand.length > 0) {
-	// 		cardImages.push(<p>first split hand score:{calculateScore(playerFourHand, false)}</p>);
-	// 		playerFourSplitHand.forEach((card) => {
-	// 			cardImages.push(<img src={card.image} alt={card.code} />);
-	// 		});
-	// 		cardImages.push(<p>second split hand score:{calculateScore(playerFourSplitHand, false)}</p>);
-	// 	}
-	// 	return <div className="player-hand">{cardImages}</div>;
-	// };
-
 	const displayComputerCards = () => {
 		let cardImages = [];
 		if (playerNumberTurn === 0) {
@@ -356,46 +260,6 @@ const GameTwentyOne = () => {
 		}
 		return <div className="player-hand">{cardImages}</div>;
 	};
-
-	// const handleTwist = (player) => {
-	// 	return (
-	// 		fetch('https://deckofcardsapi.com/api/deck/' + deckId + '/draw/?count=1')
-	// 			.then((res) => {
-	// 				console.log('res', res);
-	// 				// console.log('res json', res.json());
-	// 				if (res.ok !== true) {
-	// 					// handleTwist(player);
-	// 					// return;
-	// 					console.log('BROKEN!!!!');
-	// 					return res.json();
-	// 				} else {
-	// 					return res.json();
-	// 				}
-	// 			})
-	// 			// .then((results) => {
-	// 			// 	console.log('fetch results', results);
-	// 			// 	if (results.success !== true) {
-	// 			// 		handleTwist(player);
-	// 			// 	} else {
-	// 			// 		return results;
-	// 			// 	}
-	// 			// })
-	// 			.then((results) => {
-	// 				if (player === 1) {
-	// 					setPlayerOneHand((playerOneHand) => [ ...playerOneHand, results.cards[0] ]);
-	// 				} else if (player === 2) {
-	// 					setPlayerTwoHand((playerTwoHand) => [ ...playerTwoHand, results.cards[0] ]);
-	// 				} else if (player === 0) {
-	// 					setComputerHand((computerHand) => [ ...computerHand, results.cards[0] ]);
-	// 					// setComputerScore(computerScore + results.cards[0].value);
-	// 				} else if (player === 1.5) {
-	// 					setPlayerOneSplitHand((playerOneSplitHand) => [ ...playerOneSplitHand, results.cards[0] ]);
-	// 				}
-	// 				console.log('in handleTwist, player= ', player);
-	// 				return results;
-	// 			})
-	// 	);
-	// };
 
 	const handleTwist = (player) => {
 		const cardDrawn = deckOfCards[0];
@@ -409,7 +273,6 @@ const GameTwentyOne = () => {
 			setPlayerFourHand((playerFourHand) => [ ...playerFourHand, deckOfCards[0] ]);
 		} else if (player === 0) {
 			setComputerHand((computerHand) => [ ...computerHand, deckOfCards[0] ]);
-			// setComputerScore(computerScore + deckOfCards[0].value);
 		} else if (player === 1.5) {
 			setPlayerOneSplitHand((playerOneSplitHand) => [ ...playerOneSplitHand, deckOfCards[0] ]);
 		} else if (player === 2.5) {
@@ -446,13 +309,9 @@ const GameTwentyOne = () => {
 			setPlayerNumberTurn(3);
 		} else if (playerNumberTurn === 3.5 && numberOfPlayers > 3) {
 			setPlayerNumberTurn(4);
-			// } else if (playerNumberTurn === 4.5 && numberOfPlayers === 4) {
-			// 	setPlayerNumberTurn(0);
-			// 	setComputerScore(calculateScore(computerHand));
 		} else if (playerNumberTurn >= numberOfPlayers) {
 			setPlayerNumberTurn(0);
 			setComputerScore(calculateScore(computerHand));
-			// handleComputerTurn();
 		} else {
 			setPlayerNumberTurn(playerNumberTurn + 1);
 		}
@@ -470,17 +329,11 @@ const GameTwentyOne = () => {
 			else if (card.value === 'ACE') {
 				numberOfAces += 1;
 				score += 11;
-				// if (score + 11 > 21) score += 1;
-				// else score += 11;
 			} else score += parseInt(card.value);
 		});
 		for (let i = 0; i < numberOfAces; i++) {
 			if (score > 21) score -= 10;
 		}
-		// if (score > 21) {
-		// 	handleStick();
-		// 	alert('You are bust!!! Better luck next time.');
-		// }
 		return score;
 	};
 
@@ -506,7 +359,6 @@ const GameTwentyOne = () => {
 				betAmount = playerOneBet / 2;
 			}
 			alert('Player One beats the Dealer and wins £' + betAmount * 2 + '!');
-			// setPlayerOneWallet(playerOneWallet + betAmount * 2);
 			playerOneBank += betAmount * 2;
 		} else if (playerOneScore === computerScore && playerOneScore <= 21) {
 			computerWins = false;
@@ -515,7 +367,6 @@ const GameTwentyOne = () => {
 				betAmount = playerOneBet / 2;
 			}
 			alert('Player One draws with the Dealer and gets £' + betAmount + '!');
-			// setPlayerOneWallet(playerOneWallet + betAmount);
 			playerOneBank += betAmount;
 		}
 		if (
@@ -587,12 +438,10 @@ const GameTwentyOne = () => {
 		) {
 			computerWins = false;
 			alert("Player One's second hand beats the Dealer and wins £" + playerOneBet + '!');
-			// setPlayerOneWallet(playerOneWallet + playerOneBet);
 			playerOneBank += playerOneBet;
 		} else if (playerOneSplitScore === computerScore && playerOneSplitScore <= 21) {
 			computerWins = false;
 			alert("Player One's second hand draws with the Dealer and gets £" + playerOneBet / 2 + '!');
-			// setPlayerOneWallet(playerOneWallet + playerOneBet / 2);
 			playerOneBank += playerOneBet / 2;
 		}
 		if (
@@ -684,23 +533,6 @@ const GameTwentyOne = () => {
 			<button className="deal-button" onClick={() => handleDrawCards()}>
 				Deal
 			</button>
-			{/* {playerOneHand.length > 0 ? <button onClick={() => handleTwist(playerNumberTurn)}>Twist</button> : null}
-			{playerOneHand.length > 0 ? <button onClick={() => handleStick()}>Stick</button> : null}
-			<select id="bet-dropdown" onChange={(event) => handlePlaceBet(parseInt(event.target.value))}>
-				<option>Place your bet!</option>
-				<option value={0}>0</option>
-				<option value={1}>1</option>
-				<option value={2}>2</option>
-				<option value={3}>3</option>
-				<option value={4}>4</option>
-				<option value={5}>5</option>
-				<option value={6}>6</option>
-				<option value={7}>7</option>
-				<option value={8}>8</option>
-				<option value={9}>9</option>
-				<option value={10}>10</option>
-			</select> */}
-
 			<p>
 				Player One: £{playerOneWallet} - Bet: £{playerOneBet}
 			</p>
@@ -787,30 +619,6 @@ const GameTwentyOne = () => {
 						playerBet={playerFourBet}
 					/>
 				)}
-				{/* <Player
-					hand={computerHand}
-					calculateScore={calculateScore}
-					playerNumber={0}
-					computerTurn={true}
-					playerNumberTurn={playerNumberTurn}
-			
-				/> */}
-				{/* <div>
-					{displayCards(playerOneHand, 1)}
-					{calculateScore(playerOneHand, false)}
-				</div>
-				<div>
-					{displayCards(playerTwoHand, 2)}
-					{numberOfPlayers >= 2 && calculateScore(playerTwoHand, false)}
-				</div>
-				<div>
-					{displayCards(playerThreeHand, 3)}
-					{numberOfPlayers >= 3 && calculateScore(playerThreeHand, false)}
-				</div> */}
-				{/* <div>
-					{displayCards(playerFourHand, 4)}
-					{numberOfPlayers >= 4 && calculateScore(playerFourHand, false)}
-				</div> */}
 				<div>
 					<p>Dealer</p>
 					{computerHand.length > 0 && displayComputerCards()}
